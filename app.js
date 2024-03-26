ed909c707400883738078e0ca94fb04b24bff85c
mohammad2rghdd <mohammad2rghdd@gmail.com>
2024-03-26 22:57:33 +0330

    added base logic for my app

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server is runing on port ${PORT}`);
